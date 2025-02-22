82017a1d03f3263b75475b8a2b2e2383fe94ae42
LorenzoPiombini <piombinilorenzo1987@gmail.com>
2025-02-22 11:04:02 -0500

    refactor timecard 1.00

--- src/timecard.c
+++ src/timecard.c
@@ -19,8 +19,7 @@ unsigned char clock_in(char* employee_id)
 	time_t now = time(NULL);
 
 	int fd_tcd = open_file("timecard.dat",0);
-	if(file_error_handler(1,fd_tcd) > 0)
-	{
+	if(file_error_handler(1,fd_tcd) > 0) {
 		printf("can't open timecard files. %s:%d\n", F,L-2);
 		return 0;
 	}
@@ -29,33 +28,21 @@ unsigned char clock_in(char* employee_id)
 	char* clock_in_field = "clock_in:t_l:";
 	char* emplo_field = ":employee_id:t_s:";
 	size_t len = strlen(clock_in_field) + digits + strlen(emplo_field) + strlen(employee_id) + 1;
+	char data_to_add[len];
 
-	char* data_to_add = calloc(len,sizeof(char));
-	if(!data_to_add)
-	{
-		printf("calloc failed. %s:%d.\n",F,L-3);
-		close_file(1,fd_tcd);
-		return 0;
-	} 	
-
-	if(snprintf(data_to_add,len,"%s%ld%s%s",clock_in_field,now,emplo_field,employee_id)<0)
-	{
+	if(snprintf(data_to_add,len,"%s%ld%s%s",clock_in_field,now,emplo_field,employee_id) < 0 ) {
 		printf("data_to_add failed.%s:%d.\n",F,L-2);
 		close_file(1,fd_tcd);
-		free(data_to_add);
 		return 0;
 	}
 	
 		
-	if(!__write_safe(fd_tcd,data_to_add,"timecard",NULL))
-	{
+	if(!__write_safe(fd_tcd,data_to_add,"timecard",NULL)) {
 		printf("__write_safe failed, %s:%d.\n",F,L-2);
 		close_file(1,fd_tcd);
-		free(data_to_add);
 		return 0;
 	}
 
-	free(data_to_add);
 	close_file(1,fd_tcd);
 	return 1;
 }
@@ -65,58 +52,44 @@ unsigned char clock_out(char* employee_id)
 	/* open the index file */
 	
 	int fd_tci = open_file("timecard.inx",0);
-	if(file_error_handler(1,fd_tci) > 0)
-	{
+	if(file_error_handler(1,fd_tci) > 0) {
 		printf("can't open timecard files. %s:%d\n", F,L-2);
 		return 0;
 	}	
 	
 	HashTable *ht = NULL;
 	int i = 0 , *pi = &i;
-	if(!read_indexes("timecard", fd_tci, -1, ht,pi))
-	{
+	if(!read_indexes("timecard", fd_tci, -1, ht,pi)) {
 		printf("read indexes failed, %s:%d.\n",F,L-2);
 		close_file(1,fd_tci);
 		return 0;	
 	}
 
-	size_t n = len(ht[1]);
-	if(n == 0)
-	{
+	struct Keys_ht *keys_ar = keys(&ht[1]);
+	if(!keys_ar) {
 		printf("no clock in yet.\n");
 		free_ht_array(ht,*pi);	
 		close_file(1,fd_tci);
 		return 0;
 	}
 
-	char** keys_ar = keys(&ht[1]);
-	if(!keys_ar)
-	{
-		printf("no clock in yet.\n");
-		free_ht_array(ht,*pi);	
-		close_file(1,fd_tci);
-		return 0;
-	}
-
-	int j = 0, fnd = 0, pos[n];
-	for(j = 0; j < n; j++)
-	{
-		if(strstr(keys_ar[j],employee_id) == NULL)
-		{
-			pos[j] = -1;
-			continue;
-		}else
-		{	
-			fnd++;
-			pos[j] = j;
+	int j = 0, fnd = 0, pos[keys_ar->length];
+	for(j = 0; j < keys_ar->length; j++) {
+		if(keys_ar->types[j] == STR) {
+			if(strstr((char*)keys_ar->k[j],employee_id) == NULL) {
+				pos[j] = -1;
+				continue;
+			}else{	
+				fnd++;
+				pos[j] = j;
+			}
 		}
 	}
 
-	if(fnd == 0)
-	{
+	if(fnd == 0) {
 		printf("no clock in for this employee.\n");
 		free_ht_array(ht,*pi);	
-		free_strs(n,1,keys_ar);
+		free_keys_data(keys_ar);
 		close_file(1,fd_tci);
 		return 0;
 	}
@@ -124,28 +97,24 @@ unsigned char clock_out(char* employee_id)
 	/* open the data file to perform the clockout */
 	
 	int fd_tcd = open_file("timecard.dat",0);
-	if(file_error_handler(1,fd_tcd) > 0)
-	{
+	if(file_error_handler(1,fd_tcd) > 0) {
 		printf("can't open timecard files, %s:%d.\n",F,L-3);
 		free_ht_array(ht,*pi);	
-		free_strs(n,1,keys_ar);
+		free_keys_data(keys_ar);
 		close_file(1,fd_tci);
 		return 0;
 	}
 	
 
-	for(j = 0; j < n; j++)
-	{
+	for(j = 0; j < keys_ar->length; j++) {
 		if(pos[j] == - 1)
 			continue;
 		
 
-		if(return_first_char(keys_ar[pos[j]]) == 'o')
-		{
+		if(return_first_char((char*)keys_ar->k[pos[j]]) == 'o') {
 			fnd--;
 			continue;
-		} else
-		{
+		} else {
 			break;
 		}
 
@@ -156,52 +125,38 @@ unsigned char clock_out(char* employee_id)
 		printf("employee already clocked out.\n");	
 		free_ht_array(ht,*pi);	
 		close_file(2,fd_tcd,fd_tci);
-		free_strs(n,1,keys_ar);
+		free_keys_data(keys_ar);
 		return 0;
 	}
 
  	char* clock_o_field = "clock_out:t_l:";
 	time_t now = time(NULL);
 	size_t size = number_of_digit(now) + strlen(clock_o_field) + 1;
-	char* data_to_add = calloc(size,sizeof(char));
-	if(!data_to_add)
-	{
-		printf("calloc failed. %s:%d.\n",F,L-3);
-		close_file(2,fd_tcd,fd_tci);
-		free_ht_array(ht,*pi);	
-		free_strs(n,1,keys_ar);
-		return 0;
-	} 
+	char data_to_add[size]; 
 
-	if(snprintf(data_to_add,size,"%s%ld",clock_o_field,now) < 0)
-	{
+	if(snprintf(data_to_add,size,"%s%ld",clock_o_field,now) < 0) {
 		printf("create data to update failed, %s:%d.\n",F,L-2);
 		close_file(2,fd_tcd,fd_tci);
 		free_ht_array(ht,*pi);	
-		free(data_to_add);
-		free_strs(n,1,keys_ar);
+		free_keys_data(keys_ar);
 		return 0;
 	}
 
-	if(!__update("timecard",fd_tci,fd_tcd,data_to_add,keys_ar[pos[j]]))
-	{
+	if(!__update("timecard",fd_tci,fd_tcd,data_to_add,keys_ar->k[pos[j]],keys_ar->types[pos[j]])) {
 		printf("update record failed. %s:%d.\n", F,L-2);
 		free_ht_array(ht,*pi);	
 		close_file(2,fd_tcd,fd_tci);
-		free(data_to_add);
-		free_strs(n,1,keys_ar);
+		free_keys_data(keys_ar);
 		return 0;
 	}
 	
-	free(data_to_add);
 	close_file(1,fd_tcd);
 
-	char* key_n = strdup(keys_ar[pos[j]]);
-	if(!key_n)
-	{
+	char* key_n = strdup((char*)keys_ar->k[pos[j]]);
+	if(!key_n) {
 		printf("strdup failed. %s:%d.\n",F,L-3);
 		free_ht_array(ht,*pi);	
-		free_strs(n,1,keys_ar);
+		free_keys_data(keys_ar);
 		return 0;	
 	}
 	
@@ -210,30 +165,31 @@ unsigned char clock_out(char* employee_id)
 	off_t off_v = 0;
 	key_n[0] = 'o';	
 	int j_old = j;
-	for(j = 0; j < *pi; j++)
-	{
+	for(j = 0; j < *pi; j++) {
 		Node* node = NULL;
-		if((node = delete(keys_ar[pos[j_old]],&ht[j])) == NULL)
+		if((node = delete(keys_ar->k[pos[j_old]],&ht[j],keys_ar->types[pos[j_old]])) == NULL)
 			continue;	
 		
 		if(off_v == 0)
 			off_v = node->value;
 
-		free(node->key);
+		if(node->key_type == STR)
+			free(node->key.s);
+		
 		free(node);
 
-		if(!set(key_n,off_v,&ht[j]))
-		{
+		if(!set(key_n,STR,off_v,&ht[j])) {
 			printf("set key failed, %s:%d.\n",F,L-2);
 			free_ht_array(ht,*pi);
 			free(key_n);	
-			free_strs(n,1,keys_ar);
+			free_keys_data(keys_ar);
 			close_file(1,fd_tci);
 			return 0;
 		}
 	}
 
 	free(key_n);	
+	free_keys_data(keys_ar);
 	
 	int lock_pos_i = 0, *plp_i = &lock_pos_i;
 	int lock_pos_arr_i, *plpa_i = &lock_pos_arr_i;
@@ -270,9 +226,7 @@ unsigned char clock_out(char* employee_id)
 	{
 		printf("can't open timecard files, %s:%d.\n",F,L-3);
 		free_ht_array(ht,*pi);	
-		free_strs(n,1,keys_ar);
-		if(shared_locks)
-		{/*release the locks before exit*/
+		if(shared_locks) {/*release the locks before exit*/
 			int result_i = 0;
 			do
 			{
@@ -292,7 +246,6 @@ unsigned char clock_out(char* employee_id)
 	if(!write_all_indexes(ht,fd_tci,*pi))
 	{
 		free_ht_array(ht,*pi);
-		free_strs(n,1,keys_ar);
 		close_file(1,fd_tci);
 		if(shared_locks)
 		{/*release the locks before exit*/
@@ -313,7 +266,6 @@ unsigned char clock_out(char* employee_id)
 	}
 
 	free_ht_array(ht,*pi);	
-	free_strs(n,1,keys_ar);
 	close_file(1,fd_tci);
 
 	if(shared_locks)
@@ -338,75 +290,79 @@ unsigned char list_clocked_in_employee(char*** employee_list, int* size)
 {
 	/*read time card index 1 to see the employee that are working */
 	int fd_tci = open_file("timecard.inx",0);
-	if(file_error_handler(1,fd_tci) > 0)
-	{
+	if(file_error_handler(1,fd_tci) > 0) {
 		printf("error opening timecard file %s:%d.\n",F,L-3);
 		return 0;
 	}
 
 	HashTable ht = {0,NULL};
-	if(!read_indexes("timecard",fd_tci,1,&ht,NULL))
-	{
+	if(!read_indexes("timecard",fd_tci,1,&ht,NULL)) {
 		printf("read from file failed, %s:%d.\n", F,L-2);
+		close_file(1,fd_tci);
 		return 0;
 	}
 
 	close_file(1,fd_tci);
 
-	size_t ht_size = len(ht);
-	char** keys_ar = keys(&ht);
+	struct Keys_ht *keys_ar = keys(&ht);
+	if(!keys_ar){
+		fprintf(stderr,"retrive keys form index file filed %s:%d,\n",F,L-2);
+		close_file(1,fd_tci);
+		return 0;
+	}
+
 	destroy_hasht(&ht);
 
 	int i = 0, sz = 0;
 
 	/*keeping only the employee clocked in*/
-	for(i = 0; i < ht_size; i++)
+	for(i = 0; i < keys_ar->length; i++)
 	{
-		register char c = return_first_char(keys_ar[i]);
-		if( c != 'm' )
-		{	
-			free(keys_ar[i]);
-			keys_ar[i] = NULL;
+		register char c = return_first_char((char*)keys_ar->k[i]);
+		if( c != 'm' ) {	
+			free(keys_ar->k[i]);
+			keys_ar->k[i] = NULL;
 			continue;
 		}
 		sz++;
 	}
 	
-	if(sz == 0)
-	{
+	if(sz == 0) {
 		printf("no employee to tip.\n");
-		free_strs(ht_size,1,keys_ar);
+		free_keys_data(keys_ar);
 		return NO_EMPLOYEE_CLOCKED_IN;
 	}
 
 	*size = sz;	
 	*employee_list = calloc(sz,sizeof(char*));
-	if(!employee_list)
-	{
+	if(!employee_list) {
 		printf("calloc faield, %s:%d.\n",F,L-3);
-		free_strs(ht_size,1,keys_ar);
+		free_keys_data(keys_ar);
 		return 0;
 	}
 	int j = 0;
-	for(i = 0; i < ht_size; i ++)
-	{
-		if(!keys_ar[i])
+	for(i = 0; i < keys_ar->length; i++) {
+		if(!keys_ar->k[i])
 			continue;
 
 		char* key_fnd = NULL;
-		if(!extract_employee_key(keys_ar[i],&key_fnd,TIMECARD))
-		{
+		if(!extract_employee_key((char*)keys_ar->k[i],&key_fnd,TIMECARD)) {
 			printf("extract_employee_key failed, %s:%d.\n",F,L-2);
-			free_strs(ht_size,1,keys_ar);
+			free_keys_data(keys_ar);
 			return 0;
 		}
 			
 
 		(*employee_list)[j] = strdup(key_fnd);
+		if(!(*employee_list)[j]){
+			fprintf(stderr,"strdup() fialed, %s:%d.\n",F,L-2);
+			free_keys_data(keys_ar);
+			return 0;
+		}
 		free(key_fnd);
 		j++;
 	}
 	
-	free_strs(ht_size,1,keys_ar);
+	free_keys_data(keys_ar);
 	return 1;
 }
